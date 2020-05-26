7ade51dbf8d2a86b8f5bf313d3a8436fde3f28f5
nodefortytwo <richard.burgess@dazn.com>
2020-05-26 11:42:59 +0100

    tests

--- string.go
+++ string.go
@@ -11,8 +11,20 @@ func (ss String) Index(t string) int {
     return -1
 }
 
-func (ss String) Contains(t string) bool {
+func (ss String) Includes(t string) bool {
     return ss.Index(t) >= 0
+}
+
+func (ss String) Next(i int) int {
+    i++
+    if i >= len(ss) {
+        return 0
+    }
+    return i
+}
+
+func (ss String) NextValue(i int) string {
+    return ss[ss.Next(i)]
 }
 
 func (ss String) Any(f func(string) bool) bool {
@@ -33,8 +45,8 @@ func (ss String) All(f func(string) bool) bool {
     return true
 }
 
-func (ss String) Filter(f func(string) bool) []string {
-    vsf := make([]string, 0)
+func (ss String) Filter(f func(string) bool) String {
+    vsf := make(String, 0)
     for _, v := range ss {
         if f(v) {
             vsf = append(vsf, v)
@@ -43,7 +55,7 @@ func (ss String) Filter(f func(string) bool) []string {
     return vsf
 }
 
-func (ss String) Map(f func(string) string) []string {
+func (ss String) Map(f func(string) string) String {
     vsm := make([]string, len(ss))
     for i, v := range ss {
         vsm[i] = f(v)
--- string_test.go
+++ string_test.go
@@ -13,8 +13,30 @@ func TestString (t *testing.T){
 	assert.Equal(t, String(strs).Index("apple"), 1)
 	assert.Equal(t, String(strs).Index("bogus"), -1)
 
-	assert.Equal(t, String(strs).Contains("bogus"),false)
-	assert.Equal(t, String(strs).Contains("apple"),true)
+	assert.Equal(t, String(strs).Includes("bogus"),false)
+	assert.Equal(t, String(strs).Includes("apple"),true)
+
+	assert.Equal(t, String(strs).Next(2),3)
+	assert.Equal(t, String(strs).Next(3),0)
+	assert.Equal(t, String(strs).Next(0),1)
 
 
+	assert.Equal(t, String(strs).NextValue(0),"apple")
+	assert.Equal(t, String(strs).NextValue(3),"peach")
+
+	assert.Equal(t, String(strs).Any(isPeach),true)
+	assert.Equal(t, String(strs).Filter(isPeach), String{"peach"})
+	assert.Equal(t, String(strs).Map(makePeach)[0], "peach")
+}
+
+func isPeach(s string) bool{
+	if s == "peach" {
+		return true
+	}
+
+	return false
+}
+
+func makePeach(s string) string {
+	return "peach"
 }
