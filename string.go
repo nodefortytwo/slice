749cd0fd9b99e7df08e2154d7fceac65279753ba
nodefortytwo <richard.burgess@dazn.com>
2020-06-01 17:58:21 +0100

    applying linting suggestions

--- string.go
+++ string.go
@@ -3,66 +3,77 @@ package slice
 type String []string
 
 func (ss String) Index(t string) int {
-    for i, v := range ss {
-        if v == t {
-            return i
-        }
-    }
-    return -1
+	for i, v := range ss {
+		if v == t {
+			return i
+		}
+	}
+
+	return -1
 }
 
 func (ss String) Includes(t string) bool {
-    return ss.Index(t) >= 0
+	return ss.Index(t) >= 0
 }
 
 func (ss String) Next(i int) int {
-    i++
-    if i >= len(ss) {
-        return 0
-    }
-    return i
+	i++
+	if i >= len(ss) {
+		return 0
+	}
+
+	return i
 }
 
 func (ss String) NextValue(i int) string {
-    return ss[ss.Next(i)]
+	return ss[ss.Next(i)]
 }
 
+func (ss String) Len() int           { return len(ss) }
+func (ss String) Less(i, j int) bool { return ss[i] < ss[j] }
+func (ss String) Swap(i, j int)      { ss[i], ss[j] = ss[j], ss[i] }
+
 func (ss String) Any(f func(string) bool) bool {
-    for _, v := range ss {
-        if f(v) {
-            return true
-        }
-    }
-    return false
+	for _, v := range ss {
+		if f(v) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (ss String) All(f func(string) bool) bool {
-    for _, v := range ss {
-        if !f(v) {
-            return false
-        }
-    }
-    return true
+	for _, v := range ss {
+		if !f(v) {
+			return false
+		}
+	}
+
+	return true
 }
 
 func (ss String) Filter(f func(string) bool) String {
-    vsf := make(String, 0)
-    for _, v := range ss {
-        if f(v) {
-            vsf = append(vsf, v)
-        }
-    }
-    return vsf
+	vsf := make(String, 0)
+
+	for _, v := range ss {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+
+	return vsf
 }
 
 func (ss String) Map(f func(string) string) String {
-    vsm := make([]string, len(ss))
-    for i, v := range ss {
-        vsm[i] = f(v)
-    }
-    return vsm
+	vsm := make([]string, len(ss))
+	for i, v := range ss {
+		vsm[i] = f(v)
+	}
+
+	return vsm
 }
 
 func (ss String) Slice() []string {
-    return ss
+	return ss
 }
--- string_test.go
+++ string_test.go
@@ -1,45 +1,38 @@
-package slice
+package slice_test
 
 import (
 	"testing"
 
+	"github.com/nodefortytwo/slice"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestString (t *testing.T){
-	var strs = []string{"peach", "apple", "pear", "plum"}
+func TestString(t *testing.T) {
+	var strs = slice.String{"peach", "apple", "pear", "plum"}
 
+	assert.Equal(t, strs.Index("apple"), 1)
+	assert.Equal(t, strs.Index("bogus"), -1)
 
-	assert.Equal(t, String(strs).Index("apple"), 1)
-	assert.Equal(t, String(strs).Index("bogus"), -1)
+	assert.Equal(t, strs.Includes("bogus"), false)
+	assert.Equal(t, strs.Includes("apple"), true)
 
-	assert.Equal(t, String(strs).Includes("bogus"),false)
-	assert.Equal(t, String(strs).Includes("apple"),true)
+	assert.Equal(t, strs.Next(2), 3)
+	assert.Equal(t, strs.Next(3), 0)
+	assert.Equal(t, strs.Next(0), 1)
 
-	assert.Equal(t, String(strs).Next(2),3)
-	assert.Equal(t, String(strs).Next(3),0)
-	assert.Equal(t, String(strs).Next(0),1)
+	assert.Equal(t, strs.NextValue(0), "apple")
+	assert.Equal(t, strs.NextValue(3), "peach")
 
+	assert.Equal(t, strs.Any(isPeach), true)
+	assert.Equal(t, strs.Filter(isPeach), slice.String{"peach"})
+	assert.Equal(t, strs.Map(makePeach)[0], "peach")
 
-	assert.Equal(t, String(strs).NextValue(0),"apple")
-	assert.Equal(t, String(strs).NextValue(3),"peach")
-
-	assert.Equal(t, String(strs).Any(isPeach),true)
-	assert.Equal(t, String(strs).Filter(isPeach), String{"peach"})
-	assert.Equal(t, String(strs).Map(makePeach)[0], "peach")
-
-
-	strings := String{"awesome"}
-	assert.Equal(t,strings.Slice(), []string{"awesome"})
-
+	strings := slice.String{"awesome"}
+	assert.Equal(t, strings.Slice(), []string{"awesome"})
 }
 
-func isPeach(s string) bool{
-	if s == "peach" {
-		return true
-	}
-
-	return false
+func isPeach(s string) bool {
+	return s == "peach"
 }
 
 func makePeach(s string) string {
